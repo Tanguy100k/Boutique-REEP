75159a4c839022c92aaa64628b11da1982c19b33
Tanguy Peeren <skyguy@SkyGuys-Mac-mini.local>
2025-09-03 22:57:40 +0200

    styling license-selection-component

--- src/app/components/license-selection-component/license-selection-component.ts
+++ src/app/components/license-selection-component/license-selection-component.ts
@@ -1,4 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+type FilesCategory = 'MP3' | 'WAV' | 'STEMS';
+
+// Interface pour typer la license
+interface License {
+  name: string;
+  price: number;
+  files: FilesCategory[];
+}
 
 @Component({
   selector: 'app-license-selection-component',
@@ -8,4 +18,52 @@ import { Component } from '@angular/core';
 })
 export class LicenseSelectionComponent {
 
+  ngAfterViewInit(): void {
+    const btn = document.querySelector('button.arrow');
+
+    btn!.addEventListener('click', () => {
+    btn!.classList.toggle('flip'); // ajoute/enlève la classe à chaque clic
+});
+  }
+
+  licenses: License[] = [
+    {
+      name: 'Basic',
+      price: 23.99,
+      files: ['MP3']
+    },
+    {
+      name: 'Standard',
+      price: 29.99,
+      files: ['MP3', 'WAV']
+    },
+    {
+      name: 'Trackout',
+      price: 49.99,
+      files: ['MP3', 'WAV', 'STEMS']
+    },
+    {
+      name: 'Unlimited',
+      price: 99.99,
+      files: ['MP3', 'WAV', 'STEMS']
+    },
+    {
+      name: 'Exclusive',
+      price: 999.99,
+      files: ['MP3', 'WAV', 'STEMS']
+    },
+  ];
+
+  selectedLicense: License = this.licenses[0];
+  isDeployed: boolean = false;
+
+
+  getFilesString(files: FilesCategory[]): string {
+    return files.join(' | ');
+  }
+
+  onLicenseSelect(license: License) {
+    this.selectedLicense = license;
+    console.log(this.selectedLicense)
+  }
 }

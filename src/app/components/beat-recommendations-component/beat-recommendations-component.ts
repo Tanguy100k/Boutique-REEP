e1f42fe57450761ef5fa569ed0aeb9822c8a6bb0
Tanguy Peeren (Mac Mini) <tanguy.peeren@gmail.com>
2025-09-20 16:18:17 +0200

    wav -> mp3

--- src/app/components/beat-recommendations-component/beat-recommendations-component.ts
+++ src/app/components/beat-recommendations-component/beat-recommendations-component.ts
@@ -19,7 +19,7 @@ export class BeatRecommendationsComponent {
     name: 'Fast Car',
     description: 'Trap x RnB x Drill',
     imgUrl: 'https://i.pinimg.com/1200x/ad/93/d0/ad93d06036af4585897261d753846046.jpg',
-    audioUrl: 'Sample.wav',
+    audioUrl: 'Prod1.mp3',
     key: 'Fm',
     bpm: '140',
     date: 'Sep 2, 2025'
@@ -29,7 +29,7 @@ export class BeatRecommendationsComponent {
     name: 'Slow Car',
     description: 'Afro x Electro',
     imgUrl: 'https://i.pinimg.com/1200x/aa/8e/a3/aa8ea3536ae76bc9a3e13e5d0d1c802e.jpg',
-    audioUrl: 'Sample2.wav',
+    audioUrl: 'Prod2.mp3',
     key: 'Em',
     bpm: '126',
     date: 'Aug 4, 2025'
